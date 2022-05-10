22f28da2315311e2390f8ae8e6e57ef942f1498f
Kirti Adikane <71521028+kirtiadikane@users.noreply.github.com>
2022-05-10 18:27:00 +0530

    Create ValidPerfectSquare.java
    https://leetcode.com/problems/valid-perfect-square/
    367. Valid Perfect Square

---
+++ ValidPerfectSquare.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/valid-perfect-square/
+// 367. Valid Perfect Square
+
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int start = 1;
+        int end = num;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            long sq = (long)mid * (long)mid;   // this one is imp (long) datatype otherwise gives time limit exceeded error
+            
+            if(sq > num){
+                end = mid - 1;
+            }
+            else if(sq < num){
+                start = mid + 1;
+            }
+            else{     //sq == num
+                return true;
+            }
+        }
+        return false;
+    }
+}
