7d38d3799850830f7bc59e12f4a0dbbb1bf3d78f
Kirti Adikane <71521028+kirtiadikane@users.noreply.github.com>
2022-05-13 15:30:09 +0530

    Create FlippingImage.java
    https://leetcode.com/problems/flipping-an-image/
    832. Flipping an Image

---
+++ FlippingImage.java
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/flipping-an-image/ 
+//832. Flipping an Image
+
+
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int n = image.length;
+		for(int row = 0; row < n; row++) {
+        	for(int col=0; col < (n+1) / 2 ; col++)
+            {
+                int temp = image[row][col];
+                image[row][col] = 1-image[row][n-col-1];
+                image[row][n-col-1] = 1-temp; 
+            }
+        }
+		return image;
+    }
+}
+
+//OR
+/*
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int n = image.length;
+		int[][] arr = new int[n][n];
+
+        for(int row = 0; row < n; row++) {
+        	for(int col = 0; col < n; col++) {
+        		arr[row][col] = image[row][n-1-col];
+        	}
+        }
+        for(int row = 0; row < n; row++) {
+        	for(int col = 0; col < n; col++) {
+        		if(arr[row][col] == 0) {
+        			arr[row][col] = 1;
+        		}else {
+        			arr[row][col] = 0;
+        		}
+        	}
+        }
+		return arr;
+    }
+}
+
+*/
