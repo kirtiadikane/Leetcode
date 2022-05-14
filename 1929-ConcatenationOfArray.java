7cf150d7bcdd2bcb444088b776b322fad7e037b4
Kirti Adikane <71521028+kirtiadikane@users.noreply.github.com>
2022-05-14 15:38:51 +0530

    Create 1929-ConcatenationOfArray.java

---
+++ 1929-ConcatenationOfArray.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/concatenation-of-array/
+//1929. Concatenation of Array 
+//Easy
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2*n];
+        for(int i=0; i<n; i++){
+            ans[i] = ans[i+n] = nums[i];
+        }
+        return ans;
+    }
+}
