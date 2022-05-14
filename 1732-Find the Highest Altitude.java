ac4eeaf1065fbdb4f3d59eb2024e1fdbe8f0d066
Kirti Adikane <71521028+kirtiadikane@users.noreply.github.com>
2022-05-14 17:18:02 +0530

    Create 1732-Find the Highest Altitude.java
    https://leetcode.com/problems/find-the-highest-altitude/

---
+++ 1732-Find the Highest Altitude.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+       int max=0, temp = 0;       
+        for(int i = 0; i < gain.length; i++){         
+           temp = temp + gain[i];
+           if( temp > max ){
+                max = temp;
+           }
+        }
+        return max; 
+    }
+}
