199078f6357cbef491522c86ccc7b05301b83a8b
Kirti Adikane <71521028+kirtiadikane@users.noreply.github.com>
2022-05-14 17:12:41 +0530

    Create 1832-Check if the Sentence Is Pangram.java
    https://leetcode.com/problems/check-if-the-sentence-is-pangram/

---
+++ 1832-Check if the Sentence Is Pangram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        Set<Character> set = new HashSet<>();
+        
+        /*
+        for(char c: sentence.toCharArray()){
+            set.add(c);
+        } 
+        */
+        
+        for(int i=0; i<sentence.length(); i++){
+            set.add(sentence.charAt(i));
+        }
+        
+        return set.size() == 26; 
+        
+        
+    }
+}
