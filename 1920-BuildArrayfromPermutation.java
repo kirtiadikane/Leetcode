32e09714631a8506a168bd6de5e8f70c156b7533
Kirti Adikane <71521028+kirtiadikane@users.noreply.github.com>
2022-05-14 15:35:44 +0530

    Create 1920-BuildArrayfromPermutation.java
    1920. Build Array from Permutation

---
+++ 1920-BuildArrayfromPermutation.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/build-array-from-permutation/
+// 1920. Build Array from Permutation
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
