e6956e930a9722c85565ce5460917cca80538bdd
Kirti Adikane <71521028+kirtiadikane@users.noreply.github.com>
2022-05-18 22:42:23 +0530

    Create 1678-Goal Parser Interpretation.java
    https://leetcode.com/problems/goal-parser-interpretation/

---
+++ Easy/1678-Goal Parser Interpretation.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder str= new StringBuilder();
+        for(int i = 0; i < command.length(); i++) {
+          char c = command.charAt(i);
+        	if(c == 'G' ) {
+                str.append('G');
+            }
+            else if(c == '(' )
+            {
+                if (command.charAt(i+1) == ')' ) {
+                    str.append('o');
+        		    i++;
+                }
+        		else {
+        		   str.append("al");
+        		   i+=3;
+        	    }
+            }
+           
+        }
+         return str.toString();
+    }   
+}
