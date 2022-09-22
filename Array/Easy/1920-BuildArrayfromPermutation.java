adddeeef8bc62f18efb09e8dc8b541571ca1987d
Kirti Adikane <71521028+kirtiadikane@users.noreply.github.com>
2022-09-22 22:27:33 +0530

    Update and rename 1920-BuildArrayfromPermutation.java to Array/Easy/1920-BuildArrayfromPermutation.java

--- Array/Easy/1920-BuildArrayfromPermutation.java
+++ Array/Easy/1920-BuildArrayfromPermutation.java
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/build-array-from-permutation/
 // 1920. Build Array from Permutation
+
 class Solution {
     public int[] buildArray(int[] nums) {
         int n = nums.length;
