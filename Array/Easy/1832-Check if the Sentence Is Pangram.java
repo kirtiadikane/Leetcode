27ce22a9005f1a0bbf39793414260f4522b196bb
Kirti Adikane <71521028+kirtiadikane@users.noreply.github.com>
2022-09-22 22:26:49 +0530

    Update and rename 1832-Check if the Sentence Is Pangram.java to Array/Easy/1832-Check if the Sentence Is Pangram.java

--- Array/Easy/1832-Check if the Sentence Is Pangram.java
+++ Array/Easy/1832-Check if the Sentence Is Pangram.java
@@ -1,3 +1,5 @@
+https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+
 class Solution {
     public boolean checkIfPangram(String sentence) {
         Set<Character> set = new HashSet<>();
