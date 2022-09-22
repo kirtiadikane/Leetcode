95f6086a969388e0ae158495ddb4aec0fec76ece
Kirti Adikane <71521028+kirtiadikane@users.noreply.github.com>
2022-09-22 22:21:02 +0530

    Update and rename 1431-Kids With the Greatest Number of Candies.java to Array/Easy/1431-Kids With the Greatest Number of Candies.java

--- Array/Easy/1431-Kids With the Greatest Number of Candies.java
+++ Array/Easy/1431-Kids With the Greatest Number of Candies.java
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int max = candies[0];
